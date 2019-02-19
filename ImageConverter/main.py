12df9592036e1d6e53eabc16d5bd3adb3268814e
ps24 <31865520+ps24@users.noreply.github.com>
2019-02-19 15:38:42 -0800

    Update main to call Store_Data module

--- ImageConverter/main.py
+++ ImageConverter/main.py
@@ -1,5 +1,9 @@
-from imageToText import Read
+from imageToText import Read, Store_Data
 
 filePath = input("Enter file location: ")
-ingredients = Read(filePath)
+foodInput = input("Enter food name: ")
+ingredients = Read(filePath, foodInput)
 print(ingredients.ocr())
+
+test = Store_Data(filePath, foodInput)
+print(test.dictionary_add())
